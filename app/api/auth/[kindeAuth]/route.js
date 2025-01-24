0320958174a44f6d44a72590525fc2fe3a5dead3
Mohammad Abu Nayem <mohammadabunayem106@gmail.com>
2025-01-24 12:34:59 +0600

    make changes in kinde setup

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,6 +1,10 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function GET(request, {params}) {
-  const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint);
+export default async function handler(req, res) {
+  try {
+    await handleAuth(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
