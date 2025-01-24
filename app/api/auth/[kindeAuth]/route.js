d44020ba076d982c9d3a3445827c540b1469a5a6
Mohammad Abu Nayem <mohammadabunayem106@gmail.com>
2025-01-24 12:45:03 +0600

    chanes in the kinde setting

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -1,19 +1,6 @@
-// pages/api/auth/kinde_callback.js
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default async function handler(req, res) {
-  try {
-    // Check for both GET and POST methods
-    if (req.method === "POST" || req.method === "GET") {
-      // Handle the Kinde auth callback
-      await handleAuth(req, res);
-    } else {
-      // If the request method is not allowed, return 405 error
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).json({ message: "Method Not Allowed" });
-    }
-  } catch (error) {
-    console.error("Error in Kinde callback:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+export async function GET(request, {params}) {
+  const endpoint = params.kindeAuth;
+  return await handleAuth(request, endpoint);
 }
